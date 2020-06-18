61d87d5df0374ca541cb021a94f5aa9cb6392d78
HimanshuPandey <himanshugithub6201@gmail.com>
2020-06-18 22:44:34 +0530

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+export interface User {
+  name: string;
+  role: string;
+  isActive: boolean;
+  status: string;
+  userId: string;
+}
+export interface Message {
+  message: string;
+  sender: string;
+  receiver: string;
+  timestamp: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'XpertLandProject';
+  title = "XpertLandProject";
+  users: User[] = [
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "him@gmail.com",
+      isActive: false,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "ram@gmail.com",
+      isActive: true,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "rohit@gmail.com",
+      isActive: false,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "kolk@gmail.com",
+      isActive: true,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "su@gmail.com",
+      isActive: false,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "mum@gmail.com",
+      isActive: true,
+      role: "student",
+    },
+    {
+      name: "himanshu",
+      status: "I am good",
+      userId: "ban@gmail.com",
+      isActive: false,
+      role: "student",
+    },
+  ];
+
+  currentChating: User;
+
+  selectedChat(user: User) {
+    this.currentChating = user;
+  }
+  messages: Message[] = [
+    {
+      message: "Hi",
+      sender: "himanshu@gmail.com",
+      receiver: "ram@gmail.com",
+      timestamp: "22:40",
+    },
+    {
+      message: "Hi",
+      sender: "ram@gmail.com",
+      receiver: "himanshu@gmail.com",
+      timestamp: "22:40",
+    },
+  ];
+
+  loginUser: User = {
+    name: "himanshu",
+    status: "I am good",
+    userId: "himanshu@gmail.com",
+    isActive: true,
+    role: "student",
+  };
 }
